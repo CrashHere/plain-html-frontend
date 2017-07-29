fe620d630a2f8fc9ec413afe67eb7e85ae375047
Ilia Sidorenko <ilya@sidorenko.in>
2017-07-29 12:07:31 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+let algoliasearch = require('algoliasearch');
+let client = algoliasearch('7SG71R3MGX', '6536717a06b5e0e332e909e22eac2aa9');
+let index = client.initIndex('Emergency Housing Services');
+
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/search', (req, res) => {
+	res.render('search');
+});
+
+app.post('/search', (req, res) => {
+	index.search(req.params.search, (err, content) => {
+		res.render('search', { content })
+	});
+});
+
+app.listen(3000, function () {
+	console.log('Crash Here app listening on port 3000!')
+});
