aedc7446c0c3648e197cbdcf19b3aa0654adeb1f
David Christie <davidchristie.nz@gmail.com>
2017-07-29 16:40:57 +1200

    Perform SMS search

--- routes/sms.js
+++ routes/sms.js
@@ -1,13 +1,30 @@
 const express = require('express')
 const MessagingResponse = require('twilio').twiml.MessagingResponse
 
+const index = require('../algolia/emergencyHousingServices')
+
 const router = new express.Router()
 
 router.post('/', (req, res) => {
-  const twiml = new MessagingResponse()
-  twiml.message('The Robots are coming! Head for the hills!')
-  res.writeHead(200, {'Content-Type': 'text/xml'})
-  res.end(twiml.toString())
+  const query = req.body.Body
+  index.search(query, (error, content) => {
+    if (error) {
+      res.status(500).send('Database error')
+    }
+    const firstHit = content.hits[0]
+    let message = ''
+    if (firstHit) {
+      const { address, name, phone } = firstHit
+      message = `Your closest shelter is ${name}${address ? 'at ' + address : ''}. It can be reached at ${phone}.`
+    } else {
+      message = `No shelters found.`
+    }
+    console.log(message)
+    const twiml = new MessagingResponse()
+    twiml.message(message)
+    res.writeHead(200, {'Content-Type': 'text/xml'})
+    res.end(twiml.toString())
+  })
 })
 
 module.exports = router
