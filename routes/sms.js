dfa1a7f80dd94f679d1739247fa8c6196c31a728
David Christie <davidchristie.nz@gmail.com>
2017-07-30 14:43:27 +1200

    Refactor SMS route

--- routes/sms.js
+++ routes/sms.js
@@ -1,30 +1,29 @@
 const express = require('express')
 const MessagingResponse = require('twilio').twiml.MessagingResponse
 
-const index = require('../algolia/emergencyHousingServices')
+const performLocationSearch = require('../algolia/performLocationSearch')
 
 const router = new express.Router()
 
 router.post('/', (req, res) => {
   const query = req.body.Body
-  index.search(query, (error, content) => {
-    if (error) {
-      res.status(500).send('Database error')
-    }
-    const firstHit = content.hits[0]
-    let message = ''
-    if (firstHit) {
-      const { address, name, phone } = firstHit
-      message = `Your closest shelter is ${name}${address ? 'at ' + address : ''}. It can be reached at ${phone}.`
-    } else {
-      message = `No shelters found.`
-    }
-    console.log(message)
-    const twiml = new MessagingResponse()
-    twiml.message(message)
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString())
-  })
+  return performLocationSearch(query)
+    .then(({ content }) => {
+      console.log(content)
+      const firstHit = content.hits[0]
+      let message = ''
+      if (firstHit) {
+        const { address, name, phone } = firstHit
+        message = `Your closest shelter is ${name}${address ? ' at ' + address : ''}. It can be reached at ${phone}.`
+      } else {
+        message = `No shelters found. People in immediate need of somewhere to stay should call Work and Income on 0800 559 009 or call into their nearest Ministry of Social Development service centre.`
+      }
+      console.log(message)
+      const twiml = new MessagingResponse()
+      twiml.message(message)
+      res.writeHead(200, {'Content-Type': 'text/xml'})
+      res.end(twiml.toString())
+    })
 })
 
 module.exports = router
